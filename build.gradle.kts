f24d987a9cd145927bb6b0972a81a28afa3af185
RenatoPaz <renatogomesdapaz01@gmail.com>
2025-04-14 21:33:30 -0600

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,23 @@
+// Top-level build file
+
+plugins {
+    kotlin("android") version "1.9.20" apply false
+    kotlin("kapt") version "1.9.20" apply false
+}
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.1")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
